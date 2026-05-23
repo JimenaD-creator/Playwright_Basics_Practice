bb12ce5c1990bc20bed38231c55ce135f60fa4c4
JimenaDiazFranco <a01403295@tec.mx>
2026-05-22 20:11:16 -0600

    Finish Playwright Basics Exercises

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect, firefox, chromium } from '@playwright/test';
 
 test.describe('Playwright Basics', () => {
-  test('Should verify credential error when sign in on GitHub', async({page}) => {
+  test('Should display a message error when sign in on GitHub with invalid credentials', async({page}) => {
     await page.goto('https://github.com/');
     await page.getByRole('link', { name: 'Sign in' }).click();
     await page.getByLabel('Username or email address').fill('jimena@test.com');
@@ -12,7 +12,7 @@ test.describe('Playwright Basics', () => {
     await expect(errorMessage).toBeVisible();
   })
 
-  test('Should displays context length', async({}) => {
+  test('Should display context length', async({}) => {
     const browser = await firefox.launch({});
     const context = await browser.newContext()
     console.log(`Browser context length: ${browser.contexts().length}`);
@@ -30,12 +30,15 @@ test.describe('Browser Context with multiple pages', () => {
   test('Should manage multiple pages in a context', async() => {
     const browser = await chromium.launch()
     const context = await browser.newContext()
-    const page1 = await browser.newPage()
+    const page1 = await context.newPage()
     await page1.goto('https://playwright.dev/docs/intro')
-    const page2 = await browser.newPage()
+    const page2 = await context.newPage()
     await page2.goto('https://playwright.dev/docs/writing-tests')
 
     const allPages = context.pages()
+    console.log(`Total pages in context: ${allPages.length}`);
+
+    console.assert(allPages.length === 2, 'Should have 2 pages in the context');
 
     await page1.screenshot({ path: './screenshots/installation-page.png' });
     await page2.screenshot({ path: './screenshots/writing-tests-page.png' });
@@ -46,19 +49,20 @@ test.describe('Browser Context with multiple pages', () => {
   })
 })
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe("Pages Methods", () => {
+  test("Should navigate through page history", async() => {
+    const browser = await firefox.launch();
+    const context = await browser.newContext()
+    const page = await browser.newPage();
+    await page.goto('https://playwright.dev/')
+    await page.screenshot({path: "./screenshots/screenshot_playwright.png"})
+    page.once('load', () => console.log('Page loaded!'));
+    await page.goto('https://github.com/')
+    await page.screenshot({path: "./screenshots/screenshot_github.png"})
+    await page.goBack()
+    await page.screenshot({ path: './screenshots/after-navigation-back.png' });
+    await context.close()
+    await browser.close();
+  })
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+})
